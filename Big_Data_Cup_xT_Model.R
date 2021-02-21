27c1bf08f2a43cd6af8ea998e535f66a95d4f4a0
Avery Ellis <averycellis01@gmail.com>
2021-02-21 15:55:39 -0600

    cutting code
    taking out plotting stuff and some other small things that we won't use

--- Big_Data_Cup_xT_Model.R
+++ Big_Data_Cup_xT_Model.R
@@ -1,46 +1,11 @@
 #Big Data Cup xTT Model
 #Avery Ellis and Matt Hurley
-#2/16/2021
-
-#Quick Notes on xTT
-#From Soccer: Singh [2019]
-#Location-based Markov model that tries to describe value of any point on the pitch [this time: ice]
-#Assumes 3 possible actions from any location: move, pass, shoot
-#Uses naive location-based value for shot
-#Uses Markov transition matrix to iteratively determine value of pass/move based on chances of moving to more valuable location
-#Sportlogiq [2020]: what I'm calling xTT
-#Same as soccer model, but added in term for turnover
-#Giveaway is simply the inverse; i.e a giveaway's negative is equivalent to the gain an opponent would get by being there
-#So for given (i, j) on the rink:
-#xTT(i,j) = (p(Score at i,j) * p(Shoot from i,j))+ 
-#p(skate or pass from i, j) * sum over all other zones of (p(passing or skating to a zone) * xTT(that zone)) -
-#p(lose the puck from i, j) * sum over all other zones of (p(failing a pass to a zone) * xTT(that zone))
-#A player's xTT is defined as the sum of all xT they create in individual actions (positive and negative)
-
-#Why Is This Useful?
-#Already done for NHL (SportLogiq), so why implement it here?
-#Evaluate where prospects are above-average in creating threat--
-#i.e. do they pass to valuable places? do they move to valuable places?
-#While certainly not expected in NHL immediately (for most), can be a useful gauge relative to NHL team's xT
-#do they create threat by making useful passes or valuable shots?
-#If future data becomes available, tracking change in threat over leagues/time could be HUGE
-#Attempts to quantify "hockey IQ" --i.e. how often does this player choose to make a play that adds value?
-#Compare general OHL to NHL also valuable--understanding how different areas have different 
-#threat levels at different competition levels
-#The model itself isn't necessarily new, but using it as a tool to evaluate prospects is
-#Also--maybe use k-means to cluster threat generation?
-#i.e. value added via passing, carrying, etc to characterize playstyles
-#And definitely more applications--gotta keep thinking
-
-#Maybe when finished running the model compare results to the person on Twitter running VAEP? 
-#Curious to see what results he gets
-#idk even if the dataset is large enough to run ML, it's only 40 games so I don't think so but would be a useful comparison
+#2/21/2021
 
 
 
 
 #PART 1: SETUP
-#(libraries I'll use, downloading data, subsetting data into what we'll use, etc.)
 
 library(tidyverse)
 library(ggforce)
@@ -54,11 +19,9 @@ scouting_dataset$Y.Coordinate <- as.character(scouting_dataset$Y.Coordinate)
 
 model_events <- scouting_dataset %>%
   subset({Event == "Shot" | Event == "Play" | Event == "Goal" | Event == "Takeaway"})
-  # subset({Event == "Play" | Event == "Incomplete Play" | Event == "Shot" | Event == "Goal" | Event == "Takeaway"})
 #this pulls all the data from the scouting dataset and then subsets it to all our events we use for the xT model
 
-#Below times how long it takes to make the carries
-start_time <- Sys.time()
+
 #Creating "Carries" --i.e. moving with the puck, based on the event data given
 for (row in 1:nrow(scouting_dataset)) {
   if ({scouting_dataset[row, ]$Event %in% c("Puck Recovery", "Takeaway")} & 
@@ -75,8 +38,6 @@ for (row in 1:nrow(scouting_dataset)) {
       )
   }
 }
-end_time <- Sys.time()
-print(end_time-start_time)
 
 #these loops do the same thing--checking to see where consecutive events happen and then creating a "carry" event
 #based on the location difference
@@ -122,39 +83,16 @@ for (row in (1:nrow(scouting_dataset))) {
 model_events <- model_events %>% mutate_at(
   vars(contains("Coordinate")), funs(as.numeric())
 )
+#should run the below 4 lines in 1
 model_events$X.Coordinate <- as.numeric(model_events$X.Coordinate)
 model_events$X.Coordinate.2 <- as.numeric(model_events$X.Coordinate.2)
 model_events$Y.Coordinate <- as.numeric(model_events$Y.Coordinate)
 model_events$Y.Coordinate.2 <- as.numeric(model_events$Y.Coordinate.2)
 
 
-#PART 2: INITIAL PLOTTING
-#just to visualize where everything happens
 
-model_events_plotting <- model_events
-model_events_plotting$Event = as.factor(model_events_plotting$Event)
-model_events_plotting$X.Coordinate = (model_events_plotting$X.Coordinate - 100)
-model_events_plotting$X.Coordinate.2 = (model_events_plotting$X.Coordinate.2 - 100)
-model_events_plotting$Y.Coordinate = (model_events_plotting$Y.Coordinate - 42.5)
-model_events_plotting$Y.Coordinate.2 = (model_events_plotting$Y.Coordinate.2 - 42.5)
-#makes the original data look nice to plot it b/c otherwise it wouldn't fit on the rink plot for some reason
-
-all_model_events_viz <-
-  nhl_rink_plot()+
-  theme_void()+
-  geom_jitter(data = model_events_plotting, aes(x = X.Coordinate, y = Y.Coordinate, color = Event), alpha = 0.35, 
-              fill = "#100b96")+
-  scale_fill_continuous(type = "viridis")+
-  theme(plot.title = element_text(hjust = 0.5), plot.caption = element_text(hjust = 0.5, face = "italic"))+
-  labs(x = "", y = "", title = "Locations of Model Events", 
-       caption = "Viz by Avery Ellis, @aellis0; Data from Stathletes   ")
-all_model_events_viz
-
-
-
-
-#PART 3: CLEANING DATA 
-#goal here is to split our model events into 5x5 bins that cover the entire rink
+#PART 2: CLEANING DATA 
+#splitting our events into 5x5 bins that cover the entire rink
 
 model_events <- model_events %>% mutate(
     Rounded.X.Location = (X.Coordinate %/% 5),
@@ -162,11 +100,8 @@ model_events <- model_events %>% mutate(
     Rounded.X.Location.2 = (X.Coordinate.2 %/% 5),
     Rounded.Y.Location.2 = (Y.Coordinate.2 %/% 5)
   )
-#divides the rink into 680 5x5 squares to condense data since we don't have enough data to get more granular
+#divides the rink into 697 5x5 squares to condense data since we don't have enough data to get more granular
 #Future Consideration w' more data: divide smaller (3x3?)
-#Singh (2019) does 12x8 bins for a soccer pitch, this seems a relatively good scaled approximation
-#although maybe we can scale to make it "square" i.e. same bins lengthwise and widthwise, but this is an easy fix
-#just change the 5 to something larger for the x-coordinate line
 
 model_events <- model_events %>% mutate(
   Bin = ifelse(
@@ -193,7 +128,7 @@ model_events <- model_events %>% mutate(
 
 
 
-#PART 4: WHERE ARE EVENTS CONCENTRATED?
+#PART 3: BIN CONCENTRATIONS
 #Finding total events per bin and then plotting to visualize where things happen
 bins_df <- tibble(Possible.Bins = c(0:696))
 bins_df <- bins_df %>% mutate(
@@ -219,71 +154,9 @@ bins_df <- bins_df %>%
   )
 #Adding in new columns with the locations of the bins; in effect working backwards to get what we had before in a new df
 
-bins_df$NumericFreq <- as.numeric(bins_df$Freq)
-bins_df$NumericFreq <- scale(bins_df$NumericFreq)
-bins_df$Freq <- as.factor(bins_df$Freq)
-#For ggplot2 to make plotting easier, this doesn't actually change anything about our observations.
-#Results in a tibble with the rink split up into 5x5 bins, and how many events happened in each bin.
-
-bins_df_plotting <- bins_df
-bins_df_plotting$Approx.X.Location = 0.97 * (bins_df_plotting$Approx.X.Location - 97.5)
-bins_df_plotting$Approx.Y.Location = (bins_df_plotting$Approx.Y.Location - 41)
-#To make our new dataframe look nice when we plot it.
-
-bins_df_viz <-
-  nhl_rink_plot()+ 
-  theme_void()+
-  geom_point(data = bins_df_plotting, aes(x = Approx.X.Location, y = Approx.Y.Location, size = NumericFreq),
-              color = "#032cfc", alpha = 0.5)+
-  theme(legend.position = "none", plot.title = element_text(hjust = 0.5), 
-        plot.caption = element_text(hjust = 0.5, face = "italic"),
-        plot.subtitle = element_text(hjust = 0.5))+
-  labs(x = "", y = "", title = "Concentrated Locations of Passes, Shots, and Carries", 
-       subtitle = "Located in Closest 5x5 Region, Scaled by Concentration",
-       caption = "Viz by Avery Ellis, @aellis0; Data via Stathletes")
-bins_df_viz
-#This is an estimation of where things were recorded on the ice; bigger circles mean more things were recorded there.
 
 
-
-
-#5: UNDERSTANDING THE MODEL SETUP
-#Let's choose a random bin (in the offensive zone) and see all events that happen there.
-
-rand_bin <- model_events %>%
-  subset(Bin == 552)
-rand_bin_plotting <- rand_bin
-rand_bin_plotting$Event = as.factor(rand_bin_plotting$Event)
-rand_bin_plotting$X.Coordinate = (rand_bin_plotting$X.Coordinate - 100)
-rand_bin_plotting$X.Coordinate.2 = (rand_bin_plotting$X.Coordinate.2 - 100)
-rand_bin_plotting$Y.Coordinate = (rand_bin_plotting$Y.Coordinate - 42.5)
-rand_bin_plotting$Y.Coordinate.2 = (rand_bin_plotting$Y.Coordinate.2 - 42.5)
-
-rand_bin_viz <-
-  nhl_rink_plot()+ 
-  theme_void()+ 
-  geom_segment(data = (rand_bin_plotting %>% filter(Event %nin% c("Shot", "Goal"))), 
-               aes(x = X.Coordinate, xend = X.Coordinate.2, y = Y.Coordinate, yend = Y.Coordinate.2, color = Event))+
-  geom_jitter(data = (rand_bin_plotting %>% filter(Event %in% c("Shot", "Goal"))), 
-             aes(x = X.Coordinate, y = Y.Coordinate, color = Event))+
-  theme(plot.title = element_text(hjust = 0.5), 
-        plot.caption = element_text(hjust = 0.5, face = "italic"),
-        plot.subtitle = element_text(hjust = 0.5))+
-  labs(x = "", y = "", title = "Events in Bin 500", subtitle = "Total Events = 35", 
-       caption = "Viz by Avery Ellis, @aellis0; Data from Stathletes")
-rand_bin_viz
-#So we've got like 6 passes and a bunch of shots, and a singular goal.
-#Our xT(sub i) at a given location is defined as having three parts:
-#first, the probability of scoring a goal from that location
-#second, the probability of scoring after i iterations
-#third, the likelihood of a turnover in those i iterations followed by an opponent scoring.
-#So let's run the first iteration, which is the probability of scoring in 1 iteration. 
-#This is basically a VERY NAIVE xG model, using only location-based data, and even more naive since it's just location-based. 
-
-
-
-
-#6: THE FIRST ITERATION
+#PART 4: THE FIRST ITERATION
 #Building the naive xG model.
 #We do this pretty easily: take the data for all bins where the event is a shot or a goal. 
 #The probability of scoring in 1 iteration is thus Goals/(Goals + Shots).
@@ -312,7 +185,7 @@ iter_1_goals_bins <- iter_1_goals_bins %>%
   rename(Goals = Freq)
 names(iter_1_goals_bins)[names(iter_1_goals_bins) == "."] <- "Bin"
 iter_1_goals_bins$Bin <- as.numeric(as.character(iter_1_goals_bins$Bin))
-#same here but with just goals, it would be nice if I knew how to write a damn function
+#same here but with just goals
 
 
 iter_1_bins <- left_join(iter_1_bins, iter_1_goals_bins)
@@ -322,52 +195,14 @@ iter_1_bins <- iter_1_bins %>%
   mutate(xTT1 = (Goals / `Total Shots`))
 #our initial xTT column, appended with 1 to indicate first iteration
 
-xTT <- left_join(bins_df, iter_1_bins)
-xTT[is.na(xTT)] = 0
-#joins into the bins_df df and replaces all the NA with 0
-
-
-#For ggplot2 to make plotting easier, this doesn't actually change anything about our observations.
-xTT_plotting <- xTT
-xTT_plotting$xTT_plotting <- scale(xTT_plotting$xTT1)
-xTT_plotting$Approx.X.Location = 0.97 * (xTT_plotting$Approx.X.Location - 97.5)
-xTT_plotting$Approx.Y.Location = (xTT_plotting$Approx.Y.Location - 41)
-#To make our new dataframe look nice when we plot it.
-
-iter_1_viz <-
-  nhl_rink_plot()+ 
-  theme_void()+
-  geom_point(data = (xTT_plotting %>% filter((`Total Shots` > 1) & (xTT1 > 0))), 
-             aes(x = Approx.X.Location, y = Approx.Y.Location, size = xTT1),
-             color = "#032cfc", alpha = 0.5)+
-  theme(legend.position = "none", plot.title = element_text(hjust = 0.5), 
-        plot.caption = element_text(hjust = 0.5, face = "italic"),
-        plot.subtitle = element_text(hjust = 0.5))+
-  labs(x = "", y = "", title = "Initial xT Model", 
-       subtitle = "Minimum 1 Goal and 2 Shots, Located in Closest 5x5 Region, Scaled by xG",
-       caption = "Viz by Avery Ellis, @aellis0; Data via Stathletes")
-iter_1_viz
-#Our initial xT model! Hey hey! 
-#Note that we filtered to exclude the regions where there weren't any shots taken or there was only one shot.
-#So what have we learned? Not a lot, cause this is a super naive model.
-#But it passes the smell test, seemingly concentrated in the slot and towards the net. So I haven't messed up yet.
-#This is again, in effect, the xT if only shooting is a possibility, and you can't move.
+xTT <- left_join(bins_df, iter_1_bins) %>%
+  replace(is.na(.), 0)
+#joins the xTT column into the bins_df and replaces all the NA with 0
 
 
 
 
-#PART 7: MORE ITERATIONS
-#Now to really define xTT.
-#As a reminder: our xTT in a given region is defined as:
-#(chance of shooting from that region * chance of scoring from that region) + 
-#(chance of moving from that region * sum over all regions of (value of that region * chance of moving to that region))-
-#(chance of losing the puck in that location * xTT of that region's opposite)
-#So let's create the xT of one bin and then apply that for every other bin.
-
-#Take bin 2. Events are: Play, Carry, Carry, Carry, Carry.
-#Thus: we say xT of bin 2 is equal to xT generated from shooting there (zero)
-#plus xT from all the zones passed to/carried to * probability of moving to each zone
-#minus the xT of that location flipped across the rink.
+#PART 5: THE SECOND ITERATION
 
 xTT <- xTT %>%
   add_column(xTT2 = 0,
@@ -413,7 +248,7 @@ xTT <- xTT %>%
 #All positive events that happen are moves (passes and carries)
 #But a takeaway is an "event", so this is failed passes and takeaways.
 
-#PART 7A: Calculating The Positive Transition Matrix 
+#PART 5A: Calculating The Positive Transition Matrix 
 for (bin in 1:nrow(xTT)) {
   #iterates through every bin
   pos_df <- model_events %>%
@@ -449,7 +284,7 @@ for (bin in 1:nrow(xTT)) {
 }
 
 
-#PART 7B: Calculating the Negative Transition Matrix
+#PART 5B: Calculating the Negative Transition Matrix
 for (bin in 1:nrow(xTT)) {
   #iterates through every bin
   neg_df <- model_events %>%
@@ -467,7 +302,7 @@ for (bin in 1:nrow(xTT)) {
   neg_df_plays <- neg_df %>%
     subset(Event == "Failed Play") %>%
     as_tibble()
-  #Part 7B1: Calculating the Negative Impact of Failed Passes
+  #Part 5B1: Calculating the Negative Impact of Failed Passes
   if (nrow(neg_df_plays) > 0) {
     neg_df_freq <- neg_df_plays$Bin.2 %>%
       table() %>%
@@ -492,7 +327,7 @@ for (bin in 1:nrow(xTT)) {
   #finally, calculate and store the total xTT from failed passes.
   }
   failed_passes_xTT <- ifelse(failed_passes_xTT != 0, failed_passes_xTT, 0)
-  #Part 7B2: Calculating the Negative Impact of Takeaways
+  #Part 5B2: Calculating the Negative Impact of Takeaways
   if (nrow(neg_df_takeaways > 0)) {
     #basically all this block does is find the flipped x-location, 
     #adjusts it to the approximate coordinates,
@@ -535,38 +370,10 @@ for (bin in 1:nrow(xTT)) {
   #of our xTT!
 }
 
-#PART 7C: Checking our Results from the Second Iteration
-xTT_plotting <- xTT
-xTT_plotting$xTT_plotting <- scale(xTT_plotting$xTT2)
-xTT_plotting$Approx.X.Location = 0.95 * (xTT_plotting$Approx.X.Location - 97.5)
-xTT_plotting$Approx.Y.Location = (xTT_plotting$Approx.Y.Location - 41)
-
-iter_2_viz <-
-  nhl_rink_plot()+ 
-  theme_void()+
-  geom_point(data = (xTT_plotting %>% filter(xTT2 > 0)), 
-             aes(x = Approx.X.Location, y = Approx.Y.Location, size = xTT2),
-             color = "#032cfc", alpha = 0.5)+
-  geom_point(data = (xTT_plotting %>% filter(xTT2 == 0)), 
-             aes(x = Approx.X.Location, y = Approx.Y.Location, size = xTT2),
-             color = "#510a8c", alpha = 0.5)+
-  geom_point(data = (xTT_plotting %>% filter(xTT2 < 0)),
-             aes(x = Approx.X.Location, y = Approx.Y.Location, size = xTT2),
-             color = "#f00a0a", alpha = 0.5)+
-  theme(legend.position = "none", plot.title = element_text(hjust = 0.5), 
-        plot.caption = element_text(hjust = 0.5, face = "italic"),
-        plot.subtitle = element_text(hjust = 0.5))+
-  labs(x = "", y = "", title = "Second Iteration of xTT Model", 
-       subtitle = "Located in Closest 5x5 Region, Scaled by xTT",
-       caption = "Viz by Avery Ellis and Matt Hurley; Data via Stathletes")
-iter_2_viz
-#Yep, that checks out--positives are concentrated towards the slot, 
-#and negatives the same across the rink
 
 
 
-
-#PART 8: MORE ITERATIONS
+#PART 6: MORE ITERATIONS
 #Singh used 5 iterations--we're gonna try that and see if our model converges to our liking.
 xTT <- xTT %>%
   mutate(xTT3 = 0,
@@ -673,14 +480,13 @@ for (iteration in 3:5) {
   }
 }  
 
-#plotting to check it out
+#plotting the xTT heatmap
 xTT_plotting <- xTT
 xTT_plotting <- xTT_plotting %>%
   mutate(
     Approx.X.Location = (0.96* (Approx.X.Location - 100)),
     Approx.Y.Location = Approx.Y.Location - 40
   )
-
 
 more_iter_viz <-
   nhl_rink_plot()+ 
@@ -698,13 +504,13 @@ more_iter_viz <-
        caption = "Viz by Avery Ellis and Matt Hurley; Data via Stathletes")
 
 more_iter_viz
-#Looks pretty damn good to me. Majority of regions are positive or negative, with a few zeroes.
+#Majority of regions are positive or (barely) negative, with a few zeroes.
 #Concentration in o-zone is in slot, with the majority of the d-zone being negative.
 
 
 
 
-#PART 9: Applying xTT
+#PART 7: Applying xTT
 model_events <- model_events %>%
   mutate(xTT = 0,
          xTT.2 = 0,
@@ -725,11 +531,12 @@ model_events <- model_events %>%
   mutate(xTT.Change = ifelse(
     (Event %in% c("Play", "Carry")), (xTT.2 - xTT),
     ifelse((Event == "Failed Play"), -(xTT.2 + xTT),
-           #ifelse((xTT.2 > xTT), -(xTT.2 - xTT), (xTT.2 - xTT)), 
            xTT))
   )
 #calculating the change in xTT from those values. 
-#Carries, Plays, and Failed Plays get you the change in xTT.
+#Carries and Plays get you the pure change in xTT.
+#Failed Plays get you the sum of the xTT, flipped negative;
+#this is because the s
 #Takeaways give you the xTT where they occur.
 
 plays <- model_events %>%
